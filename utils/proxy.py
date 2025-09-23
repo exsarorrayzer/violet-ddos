bb19e4e8a2507a9233d5d3968df14e49fd58b472
exsarorrayzer <theleapy@gmail.com>
2025-09-23 19:24:00 +0400

    Update proxy.py

--- utils/proxy.py
+++ utils/proxy.py
@@ -4,14 +4,17 @@ proxies = []
 
 def load_proxies(path="proxy.txt"):
     global proxies
-    try:                                                                        with open(path, "r") as file:
+    try:
+        with open(path, "r") as file:
             proxies = [line.strip() for line in file if line.strip()]
     except FileNotFoundError:
         proxies = []
 
 def get_random_proxy():
     global proxies
-    if not proxies:                                                             return None                                                         return random.choice(proxies)
+    if not proxies:
+        return None
+    return random.choice(proxies)
 
 def remove_proxy(proxy):
     global proxies
